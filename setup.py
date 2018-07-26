8e4f08836b09a93c25e70ddbda2635f442ff4dd0
Albert Boehmler <ajboehmler@gmail.com>
2018-07-25 23:29:13 -0400

    Adding game_lobby dependency

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ),
-    install_requires=[]
+    install_requires=["gamechain_lobby"]
 )
